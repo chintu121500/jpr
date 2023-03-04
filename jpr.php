3620650166a3d1f352757d31400085c030569983
chintu121500 <126768736+chintu121500@users.noreply.github.com>
2023-03-04 17:32:02 +0530

    first commit

---
+++ jpr.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Creat account </title>
+</head>
+<body>
+  <form>
+  <figure
+  
+	<p>
+	<h1 style="color:black;"><img src="https://naukrirecruiter.naukri.com/profilePic/getpic?pid=1577526105rp4226280_medium4  q q" width="100" height="50">ADS EXCHANGE</h1><br>
+		
+    <figure>
+     </p>
+	 <p>
+	    <h1 style="color:blue;">Get started</h1><br>
+</P>
+<p>
+It's free to signup and only takes a minute
+</p>
+         Full name :
+		 </p>
+		  <input type = "text" placeholder ="enter your full name"/>
+ 
+   <p>
+</p>
+
+Sponser id :
+</p>
+    <input type = "text" placeholder = "enter your sponser id" />
+
+</p>
+Password  : 
+
+</p>
+     <input type = "text" placeholder = "enter your password" />
+<p>
+Email id : 
+</p>
+ <input type = "text" placeholder  = "enter your Email ID" />
+<p>
+Phone Number: 
+</p>
+ <input type = "text" placeholder = "enter your phone number " />
+ <p>
+      <input type = "submit" name = "submit" value = "Create account" />
+    </p>
+</fieldset>
+</form>
+</body>
+</html>
